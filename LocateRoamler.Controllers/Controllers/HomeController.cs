23eaac7898d3e43aac806ad75128d5510f151f7e
NamJn <namrataj3210@gmail.com>
2021-09-26 20:20:08 +0530

    Added error page to be handled on prod env.

--- LocateRoamler.Controllers/Controllers/HomeController.cs
+++ LocateRoamler.Controllers/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using LocateRoamler.Services;
 using LocateRoamler.Services.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
@@ -20,6 +22,19 @@ namespace LocateRoamler.Controllers.Controllers
             Location originLocation = new Location(Convert.ToDouble(model.OriginLatitude), Convert.ToDouble(model.OriginLongitude));
             model.SearchResults = await _locationService.GetLocations(originLocation, radius, model.SelectedMaxResult);
             return View(model);
+        }
+
+        /// <summary>
+        /// Provide error view on production environment.
+        /// </summary>
+        /// <returns></returns>
+        [Route("Home/Error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+            return View("Error");
         }
     }
 }
